e9fbd2b6c2077fc8123e7fdd710e74705ea7fc27
AIMCamejo <57682706+AIMCamejo@users.noreply.github.com>
2019-11-19 20:16:45 +0000

    attemped fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,9 +3,9 @@
 
 for ( var i = 1; i <= 100; i++)
 
-    else if ( i % 3 == 0 && i % 5 == 0 )
+    if ( i % 3 == 0 && i % 5 == 0 )
         output("FizzBuzz");
-    if ( i % 3 == 0 )
+    else if ( i % 3 == 0 )
         output( "Fizz" );
     else if ( i % 5 == 0 )
         output("Buzz");
